aed64e3d858d33bb7fe759631d13b04d7f10fb21
Sarvarbek2704 <sarvarbekkozimov@gmail.com>
2022-01-04 11:10:46 +0500

    initial commit

--- database/config.py
+++ database/config.py
@@ -1,7 +1,7 @@
 import os
 
-database = 'hotel'
-user = 'postgres'
-password = os.environ['POSTGRES_PASSWORD']
-host = 'localhost'
+database = 'd2nrmd449n6ina'
+user = 'cvkpuwtzesdkxs'
+password = os.environ['f6ec0abe0df1ca15dc0006fb6c440499d8d4357a55d7175cc1b928fcdf94b878']
+host = 'ec2-176-34-222-188.eu-west-1.compute.amazonaws.com'
 port = '5432'
