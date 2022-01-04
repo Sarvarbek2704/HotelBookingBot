5f4a37d87c7fd0d949ae72c7807565c2ac27ffa6
Sarvarbek2704 <sarvarbekkozimov@gmail.com>
2022-01-04 11:14:35 +0500

    initial commit

--- database/config.py
+++ database/config.py
@@ -2,6 +2,6 @@ import os
 
 database = 'd2nrmd449n6ina'
 user = 'cvkpuwtzesdkxs'
-password = os.environ['f6ec0abe0df1ca15dc0006fb6c440499d8d4357a55d7175cc1b928fcdf94b878']
+password ='f6ec0abe0df1ca15dc0006fb6c440499d8d4357a55d7175cc1b928fcdf94b878'
 host = 'ec2-176-34-222-188.eu-west-1.compute.amazonaws.com'
 port = '5432'
